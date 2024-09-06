940603e495465f0fd60de59fe72bcdb63c1d213e
Ayush Khodankar <ayushkhodankar01@gmail.com>
2024-09-07 01:06:20 +0530

    Update ChatController.java

--- src/main/java/com/example/entity/ChatMessage.java
+++ src/main/java/com/example/entity/ChatMessage.java
@@ -20,5 +20,47 @@ public class ChatMessage {
     private User receiver;
     
     private String timestamp;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public User getSender() {
+		return sender;
+	}
+
+	public void setSender(User sender) {
+		this.sender = sender;
+	}
+
+	public User getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(User receiver) {
+		this.receiver = receiver;
+	}
+
+	public String getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(String timestamp) {
+		this.timestamp = timestamp;
+	}
+
+	
 }
 
